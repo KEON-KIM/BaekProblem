c43da5239396a1708899d3b12be1945ff75328c9
Keon <37167860+KEON-KIM@users.noreply.github.com>
2020-03-12 17:20:39 +0900

    update
    Update File

--- Untitled.py
+++ Untitled.py
@@ -1,31 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
 
 
 #2557
 print("Hello World!")
 
 
-# In[3]:
 
 
-mylist = [5,2,1,4]
-print(len(mylist))
-mylist.remove(4)
-
-print(mylist)
 
 
-# In[4]:
-
-
-for i in range(len(mylist)) :
-    print(i)
-
-
-# In[26]:
 
 
 #10817
@@ -43,9 +26,6 @@ mid = int(len(mylist)/2)
 print(mylist[mid])
 
 
-# In[23]:
-
-
 #2750
 N = int(input())
 if N<1 or N>1000:
@@ -61,25 +41,16 @@ for i in range(len(mylist)) :
             temp = mylist[j]
             mylist[j] = mylist[i]
             mylist[i] = temp
-#중복제거
+#remove 
 for k in range(len(mylist)) :
     if k+1 > len(mylist)-1:
         break
     if mylist[k] == mylist[k+1] :
         mylist.remove(mylist[k])
-#출력
+#output
+        
 for i in range(len(mylist)):
     print(mylist[i])
-    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
